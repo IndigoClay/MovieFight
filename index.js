63e63b25a071eb5792b2b31aa3bcf5d2e51f8145
indigoc <indigoclay@gmail.com>
2020-08-20 07:49:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const fetchData = async(searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/',  {
+        params: {
+            apikey: 'cfd9b6db',
+            s: searchTerm
+        }
+    });
+    if (response.data.Error) {
+        console.log("ERROR");
+        return [];
+    }
+    return response.data.Search;
+};
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class='input' />
+    <div class="dropdown">
+      <div class="dropdown-menu">
+       <div class="dropdown-content results"></div>
+      </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultWrapper = document.querySelector('.results');
+
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value);
+    resultWrapper.innerHTML = '';
+    dropdown.classList.add('is-active');
+    for (const movie of movies) {
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster ==='N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item');
+        option.innerHTML = `
+        <img src="${imgSrc}" />
+        ${movie.Title}
+        `;
+        resultWrapper.appendChild(option);
+    }
+};
+
+input.addEventListener('input', debounce(onInput, 500));
